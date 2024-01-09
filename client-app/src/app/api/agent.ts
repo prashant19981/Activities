3a83308c407db571dde6de79a1dc5f6c0b9ae918
prashant19981 <prashant19981@gmail.com>
2024-01-09 14:20:44 +0000

    Validation errors handling

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { Axios, AxiosError, AxiosResponse } from "axios";
 import { Activity } from "../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -16,10 +17,24 @@ axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 }, (error: AxiosError) => {
-    const { data, status } = error.response!;
+    const { data, status, config } = error.response as AxiosResponse;
     switch (status) {
         case 400:
-            toast.error('bad request');
+            if(config.method === "get" && Object.prototype.hasOwnProperty.call(data.errors,'id')){
+                router.navigate('/not-found');
+            }
+            if(data.errors){ // Validation error as it is nested inside data.errors
+                const modalStateErrors =[];
+                for(const key in data.errors){
+                    if(data.errors[key]){
+                        modalStateErrors.push(data.errors[key]);
+                    }
+                }
+                throw modalStateErrors.flat();
+            }
+            else{
+                toast.error(data);
+            }
             break;
         case 401:
             toast.error('unauthorized');
@@ -31,7 +46,8 @@ axios.interceptors.response.use(async response => {
             router.navigate('/not-found');
             break;
         case 500:
-            toast.error('server error');
+            store.commonStore.setServerError(data);
+            router.navigate('/server-error');
             break;
           
     }
--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -6,6 +6,7 @@ import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestErrors";
 import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 
 export const routes: RouteObject[] = [
@@ -20,6 +21,7 @@ export const routes: RouteObject[] = [
             {path:'manage/:id',element: <ActivityForm key="manage"/>},
             {path:'errors',element: <TestErrors/>},
             {path:'not-found',element: <NotFound/>},
+            {path:'server-error',element: <ServerError/>},
             {path:'*',element: <Navigate replace to='/not-found'/>},
 
 
