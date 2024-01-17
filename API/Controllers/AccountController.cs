3e196a0317db2e78a105b85aa2c311c62299ed97
prashant19981 <prashant19981@gmail.com>
2024-01-17 10:31:11 +0000

    Added API to get current user details from jwt token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 
 
+using System.Security.Claims;
 using API.DTOs;
 using API.Services;
 using Domain;
@@ -10,10 +11,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
+    
     [ApiController]
     [Route("api/[controller]")]
-    public class AccountController:ControllerBase
+    public class AccountController : ControllerBase
     {
 
         private readonly UserManager<AppUser> _userManager;
@@ -23,56 +24,75 @@ namespace API.Controllers
             _userManager = userManager;
             _tokenService = tokenService;
         }
-
+        [AllowAnonymous]
         [HttpPost("login")]
 
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
             var user = await _userManager.FindByEmailAsync(loginDto.Email);
 
-            if(user == null) return Unauthorized();
+            if (user == null) return Unauthorized();
 
-            var result = await _userManager.CheckPasswordAsync(user,loginDto.Password);
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
 
-            if(result){
-                return new UserDto{
-                    DisplayName = user.DisplayName,
-                    Image = null,
-                    Token = _tokenService.CreateToken(user),
-                    UserName = user.UserName
-                };
+            if (result)
+            {
+                return CreateUserObject(user);
             }
             return Unauthorized();
         }
-
+        [AllowAnonymous]
         [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
-            if(await _userManager.Users.AnyAsync(x=>x.UserName == registerDto.UserName)){
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
+            {
                 return BadRequest("Username is already taken");
             }
-            if(await _userManager.Users.AnyAsync(x=>x.Email == registerDto.Email)){
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+            {
                 return BadRequest("Email is already taken");
             }
 
-            var user = new AppUser{
+            var user = new AppUser
+            {
 
                 DisplayName = registerDto.DisplayName,
                 Email = registerDto.Email,
                 UserName = registerDto.UserName
             };
-            
-            var result = await _userManager.CreateAsync(user,registerDto.Password);
 
-            if(result.Succeeded){
-                return new UserDto{
-                    DisplayName = user.DisplayName,
-                    Image = null,
-                    Token = _tokenService.CreateToken(user),
-                    UserName = user.UserName
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-                };
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
             }
             return BadRequest("Problem registering user");
+
+
         }
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+        }
+
         
+        private UserDto CreateUserObject(AppUser user)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Token = _tokenService.CreateToken(user),
+                UserName = user.UserName
+
+            };
+        }
+
     }
 }
