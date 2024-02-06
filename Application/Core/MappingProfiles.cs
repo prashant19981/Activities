5043640bdfc2cb49eadd3d53ccae0f8be300a933
prashant19981 <prashant19981@gmail.com>
2024-02-06 12:32:30 +0000

    Added Followers count

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -15,17 +15,19 @@ namespace Application.Core
                 (d => d.HostUsername, o => o.
                     MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
             CreateMap<ActivityAttendee, AttendeeDto>()
-                .ForMember(d=>d.DisplayName, o=>o.MapFrom(s=>s.AppUser.DisplayName))
-                .ForMember(d=>d.Username, o=>o.MapFrom(s=>s.AppUser.UserName)) 
-                .ForMember(d=>d.Bio, o=>o.MapFrom(s=>s.AppUser.Bio))
-                .ForMember(d=> d.Image, o=> o.MapFrom(s=>s.AppUser.Photos.FirstOrDefault(x=>x.IsMain).Url));  
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d=> d.Image, o=> o.MapFrom(s=>s.Photos.FirstOrDefault(x=>x.IsMain).Url));     
-            CreateMap<Comment, CommentDto>()         
-            .ForMember(d=>d.DisplayName, o=>o.MapFrom(s=>s.Author.DisplayName))
-                .ForMember(d=>d.Username, o=>o.MapFrom(s=>s.Author.UserName)) 
-                .ForMember(d=> d.Image, o=> o.MapFrom(s=>s.Author.Photos.FirstOrDefault(x=>x.IsMain).Url));    
-                   
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
+            CreateMap<Comment, CommentDto>()
+            .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos.FirstOrDefault(x => x.IsMain).Url));
+
         }
     }
 }
---
+++ Persistence/Migrations/20240205172137_Followers.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Followers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserFollowing_AspNetUsers_ObserverId",
+                table: "UserFollowing");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserFollowing_AspNetUsers_TargetId",
+                table: "UserFollowing");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserFollowing",
+                table: "UserFollowing");
+
+            migrationBuilder.RenameTable(
+                name: "UserFollowing",
+                newName: "UserFollowings");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserFollowing_TargetId",
+                table: "UserFollowings",
+                newName: "IX_UserFollowings_TargetId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserFollowings",
+                table: "UserFollowings",
+                columns: new[] { "ObserverId", "TargetId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserFollowings_AspNetUsers_ObserverId",
+                table: "UserFollowings",
+                column: "ObserverId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserFollowings_AspNetUsers_TargetId",
+                table: "UserFollowings",
+                column: "TargetId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserFollowings_AspNetUsers_ObserverId",
+                table: "UserFollowings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserFollowings_AspNetUsers_TargetId",
+                table: "UserFollowings");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserFollowings",
+                table: "UserFollowings");
+
+            migrationBuilder.RenameTable(
+                name: "UserFollowings",
+                newName: "UserFollowing");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserFollowings_TargetId",
+                table: "UserFollowing",
+                newName: "IX_UserFollowing_TargetId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserFollowing",
+                table: "UserFollowing",
+                columns: new[] { "ObserverId", "TargetId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserFollowing_AspNetUsers_ObserverId",
+                table: "UserFollowing",
+                column: "ObserverId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserFollowing_AspNetUsers_TargetId",
+                table: "UserFollowing",
+                column: "TargetId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
