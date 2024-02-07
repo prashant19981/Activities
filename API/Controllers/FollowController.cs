a131c15039663b43ad68b0c1899e80e3bb752abd
prashant19981 <prashant19981@gmail.com>
2024-02-07 12:36:13 +0000

    Added follower/following count of a user

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Application.Activities;
+
 using Application.Followers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,8 +10,8 @@ namespace API.Controllers
         public async Task<IActionResult> Follow(string username) => 
             HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
 
-        // [HttpGet("{username}")]
-        // public async Task<IActionResult> GetFollowings(string username, string predicate) => 
-        //     HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetFollowings(string username, string predicate) => 
+            HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
     }
 }
